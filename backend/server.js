a87972a134dddf8cda882993aea4e8d244e586d9
Gopilal Anjaniya <gopilalanjaniya@gmail.com>
2026-01-27 22:04:34 +0530

    some update

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use('/api/cart',cartRouter)
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API Working.....");
+ return res.send("API Working.....");
 });
 
 app.listen(port, () => {
